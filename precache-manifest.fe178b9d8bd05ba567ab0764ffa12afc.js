93e7b535c960dd0d350238620fa2554299e65242
CPanourgias <panourgias.christos@gmail.com>
2019-08-09 15:04:53 +0300

    Updates

---
+++ precache-manifest.fe178b9d8bd05ba567ab0764ffa12afc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9e239f98f44aefb4ef274d2aeeda82b",
+    "url": "/CPanourgias/robofriends/index.html"
+  },
+  {
+    "revision": "8b26fca4efded57da1bb",
+    "url": "/CPanourgias/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "8cc5cdbefa0255555c0a",
+    "url": "/CPanourgias/robofriends/static/css/main.fcb5c01b.chunk.css"
+  },
+  {
+    "revision": "8b26fca4efded57da1bb",
+    "url": "/CPanourgias/robofriends/static/js/2.39ec0b78.chunk.js"
+  },
+  {
+    "revision": "8cc5cdbefa0255555c0a",
+    "url": "/CPanourgias/robofriends/static/js/main.c4fc5a51.chunk.js"
+  },
+  {
+    "revision": "3d5e7549258f122e6f78",
+    "url": "/CPanourgias/robofriends/static/js/runtime~main.892ca8ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/CPanourgias/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
