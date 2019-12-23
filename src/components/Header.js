7a2a5c95ac4f7dd19847b087f1e81c1047cf078b
Christos Panourgias <panourgias.christos@gmail.com>
2019-12-23 22:03:59 +0100

    Fix Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ class Header extends Component {
     render() {
         return (
             <div>
-                return <h1 className="f1">RoboFriends</h1>
+                <h1 className="f1">RoboFriends</h1>
                 <CounterButton color={'red'}/>
             </div>
         );
