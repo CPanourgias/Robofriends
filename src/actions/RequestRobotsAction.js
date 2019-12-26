a1b9a08215edacc02b35c18e744777be3713b650
Christos Panourgias <panourgias.christos@gmail.com>
2019-12-26 12:35:27 +0100

    Added api folder

--- src/actions/RequestRobotsAction.js
+++ src/actions/RequestRobotsAction.js
@@ -1,9 +1,9 @@
 import { REQUEST_ROBOTS_PENDING, REQUEST_ROBOTS_SUCCESS, REQUEST_ROBOTS_FAILURE } from "../constants";
+import { apiCall } from '../api/api'
 
 export const requestRobots = () => (dispatch) => {
   dispatch({ type: REQUEST_ROBOTS_PENDING });
-    fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
+  apiCall('https://jsonplaceholder.typicode.com/users')
         .then(data => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data }))
         .catch(error => dispatch({ type: REQUEST_ROBOTS_FAILURE, payload: error }))
 };
